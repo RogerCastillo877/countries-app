9b90034df6a35046ca355b0224e8fb3fbed19b56
RogerCastillo877 <roger.castillo877@gmail.com>
2023-05-22 22:03:54 -0500

    Adding components and routes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
   ]
 })
 export class SharedModule { }
